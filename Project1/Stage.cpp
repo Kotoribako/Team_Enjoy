158cb4d81047b19080c94e58a7735d1569708094
yoshiki-nakama <104492110+yoshiki-nakama@users.noreply.github.com>
2024-04-17 10:15:45 +0900

    ステージのソースコード更新

--- Project1/Stage.cpp
+++ Project1/Stage.cpp
@@ -1,11 +1,35 @@
 #include"Stage.h"
+#include<DxLib.h>
 
 Stage::Stage()
+{
+	
+}
+
+Stage::~Stage()
 {
 
 }
 
-Stage::~Stage()
+void Stage::Update()
+{
+	switch (NowStageNumber) // ステージ数によって遷移する
+	{
+	case 1: // １ステージ目の時
+		break;
+	case 2: // ２ステージ目の時
+		break;
+	case 3: // ３ステージ目の時
+		break;
+	}
+}
+
+void Stage::Draw()
+{
+
+}
+
+void Stage::MoveXStage()
 {
 
 }
--- Project1/Stage.h
+++ Project1/Stage.h
@@ -3,9 +3,21 @@
 class Stage
 {
 private:
+	int Stage1Image; // 1面の画像読み込み
+	int Stage2Image; // ２面の画像読み込み
+	int Stage3Image; // ３面の画像読み込み
+
+	int BlockImage; // 分割読み込み用
+
+	int NowStageNumber; // 今のステージ数
 
 public:
 	Stage();
 
 	~Stage();
+
+	void Update(); // 描画以外の更新処理を行う
+	void Draw(); // 描画しているものを更新
+
+	void MoveXStage(); // 横軸にステージを動かす
 };
