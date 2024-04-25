8e85636c2c9b40e9bf8dadde18ae5d2ead0e4a31
yoshiki-nakama <104492110+yoshiki-nakama@users.noreply.github.com>
2024-04-25 14:49:44 +0900

    なんちゃって当たり判定生成

--- Project1/Block.cpp
+++ Project1/Block.cpp
@@ -2,20 +2,19 @@
 #include"Dxlib.h"
 #include"Player.h"
 int Block::flg;
-struct Block::BLOCK block[11];
 Block::Block()
 {			  //左、右、上、下
-	block[0] = { 0,270,630,720 };
-	block[1] = { 340,440,565,615 };
-	block[2] = { 475,555,510,555 };
-	block[3] = { 510,555,470,510 };
-	block[4] = { 580,735,420,455 };
-	block[5] = { 780,845,365,415 };
-	block[6] = { 875,940,310,360 };
-	block[7] = { 950,1010,425,480 };
-	block[8] = { 980,1035,235,290 };
-	block[9] = { 1055,1120,350,410 };
-	block[10] = { 1125,1700,610,720 };
+	bloc[0] = { 0,270,630,720 };
+	bloc[1] = { 340,440,565,615 };
+	bloc[2] = { 475,555,510,555 };
+	bloc[3] = { 510,555,470,510 };
+	bloc[4] = { 580,735,420,455 };
+	bloc[5] = { 780,845,365,415 };
+	bloc[6] = { 875,940,310,360 };
+	bloc[7] = { 950,1010,425,480 };
+	bloc[8] = { 980,1035,235,290 };
+	bloc[9] = { 1055,1120,350,410 };
+	bloc[10] = { 1125,1700,610,720 };
 
 }
 
@@ -36,11 +35,11 @@ void Block::Update()
 
 void Block::Draw()
 {
-	DrawBox(px, py, px2, py2, GetColor(255, 0, 0), TRUE);
+	/*DrawBox(px, py, px2, py2, GetColor(255, 0, 0), TRUE);*/
 	for (int i = 0; i < 11; i++) {
-		DrawBox(block[i].X, block[i].Y, block[i].X2, block[i].Y2, GetColor(255, 0, 0), TRUE);
+		DrawBox(bloc[i].X, bloc[i].Y, bloc[i].X2, bloc[i].Y2, GetColor(255, 0, 0), TRUE);
 	}
-	DrawFormatString(0, 70, GetColor(255, 0, 0), "Standflg:%d", flg);
+	//DrawFormatString(0, 70, GetColor(255, 0, 0), "Standflg:%d", flg);
 }
 
 //void Block::PlayerStand()
