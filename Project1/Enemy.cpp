0ed11c7818a495a60940a4cc61bcbb00af480fcd
harufumi555 <120348465+harufumi555@users.noreply.github.com>
2024-05-09 13:24:44 +0900

    enemy用変数の作成

--- Project1/Enemy.cpp
+++ Project1/Enemy.cpp
@@ -1,1 +1,21 @@
 #include "Enemy.h"
+
+Enemy::Enemy()
+{
+	img = 0; //仮
+}
+
+void Enemy::Update()
+{
+
+}
+
+void Enemy::Draw()
+{
+
+}
+
+void Enemy::Enemyhit()
+{
+
+}
--- Project1/Enemy.h
+++ Project1/Enemy.h
@@ -1,5 +1,18 @@
 #pragma once
 class Enemy
 {
+private:
+
+	int img;
+	int enemyX, enemyY;
+	int enemyX2, enemyY2;
+	int count;
+	int Hitflg;
+	int speed;
+public:
+	Enemy();
+	void Update();
+	void Draw();
+	void Enemyhit();
 };
 
