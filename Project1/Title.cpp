220dfa99136dbe950acd4dd49d21d7e591e7f478
Kotoribako <youto-tome-k22h@stu.kbc.ac.jp>
2024-06-11 15:04:05 +0900

    a

--- Project1/Title.cpp
+++ Project1/Title.cpp
@@ -105,7 +105,7 @@ AbstractScene* Title::Update()
 			break;
 		case TITLE_MENU::RANKING:
 			PlaySoundMem(MenuSE, DX_PLAYTYPE_BACK, TRUE);
-			return new Clear;
+			return new GameOver;
 			break;
 		case TITLE_MENU::GAME_END:
 			PlaySoundMem(MenuSE, DX_PLAYTYPE_BACK, TRUE);
