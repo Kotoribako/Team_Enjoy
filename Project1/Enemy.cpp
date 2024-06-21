a1a23abd9eb328d9dbb54e941ae438085fdb67ed
Ryoya-Kouchi <ryoya-kochi-k22h@stu.kbc.ac.jp>
2024-06-21 12:50:28 +0900

    提出前の添削

--- Project1/Enemy.cpp
+++ Project1/Enemy.cpp
@@ -13,20 +13,14 @@ int   Enemy::Range;
 
 Enemy::Enemy()
 {
-	//if (LoadDivGraph("image/Dummy/enemy.png", 2, 2, 1, 64, 64, img)) {};
-	//enemyX = 580;
 	if (LoadDivGraph("image/Dummy/slime.png", 3, 3, 1, 64, 64, slimeImg)) {};
 	E_Img = slimeImg[0];
-	count = 0;
 	block = new Block();
 	player = new Player();
-	countup = 0;
-	moveX = 0;
-	direction = 0;
-	Hitflg = FALSE;
+
 	Range = 15;
 	s_enemy->Turnflg = 0;
-	Returnflg = 0;
+	s_enemy->Returnflg = 0;
 	switch (GameMain::NowStage)
 	{
 	case 1:
@@ -73,36 +67,27 @@ void Enemy::Update()
 {
 	E_FPS++;
 
-	//s_enemy[0].enemyX2 = enemyX + 30;
-	//enemyY2 = enemyY + 30;
-
-	//ex = enemyX - 15;
-	//ex2 = enemyX + 15;
-	//ey = enemyY - 15;
-	//ey2 = enemyY + 15;
-
 
 
 	Enemyhit();
-	count++;
 	switch (GameMain::NowStage)
 	{
 	case 1:
 	case 4:
 		if (s_enemy[0].enemyX == blockx[0].X2)
 		{
-			Returnflg = 1;
+			s_enemy[0].Returnflg = 1;
 		}
 		if (s_enemy[0].enemyX == blockx[0].X)
 		{
-			Returnflg = 0;
+			s_enemy[0].Returnflg = 0;
 		}
-		if (Returnflg == 0)
+		if (s_enemy[0].Returnflg == 0)
 		{
 			s_enemy[0].enemyX += 0.5f;
 			s_enemy[0].Turnflg = 1;
 		}
-		if (Returnflg == 1)
+		if (s_enemy[0].Returnflg == 1)
 		{
 			s_enemy[0].enemyX -= 0.5f;
 			s_enemy[0].Turnflg = 0;
@@ -114,18 +99,18 @@ void Enemy::Update()
 		{
 			if (s_enemy[i].enemyX == blockx[i].X2)
 			{
-				Returnflg = 1;
+				s_enemy[i].Returnflg = 1;
 			}
 			if (s_enemy[i].enemyX == blockx[i].X)
 			{
-				Returnflg = 0;
+				s_enemy[i].Returnflg = 0;
 			}
-			if (Returnflg == 0)
+			if (s_enemy[i].Returnflg == 0)
 			{
 				s_enemy[i].enemyX += 0.5f;
 				s_enemy[i].Turnflg = 1;
 			}
-			if (Returnflg == 1)
+			if (s_enemy[i].Returnflg == 1)
 			{
 				s_enemy[i].enemyX -= 0.5f;
 				s_enemy[i].Turnflg = 0;
@@ -139,18 +124,18 @@ void Enemy::Update()
 		{
 			if (s_enemy[i].enemyX == blockx[i].X2)
 			{
-				Returnflg = 1;
+				s_enemy[i].Returnflg = 1;
 			}
 			if (s_enemy[i].enemyX == blockx[i].X)
 			{
-				Returnflg = 0;
+				s_enemy[i].Returnflg = 0;
 			}
-			if (Returnflg == 0)
+			if (s_enemy[i].Returnflg == 0)
 			{
 				s_enemy[i].enemyX += 0.5f;
 				s_enemy[i].Turnflg = 1;
 			}
-			if (Returnflg == 1)
+			if (s_enemy[i].Returnflg == 1)
 			{
 				s_enemy[i].enemyX -= 0.5f;
 				s_enemy[i].Turnflg = 0;
@@ -172,7 +157,7 @@ void Enemy::Update()
 
 void Enemy::Draw()
 {
-	DrawFormatString(200, 200, GetColor(0, 0, 255), "ReTurnflg:%d",Returnflg);
+	//DrawFormatString(200, 200, GetColor(0, 0, 255), "ReTurnflg:%d",Returnflg);
 	//DrawFormatString(200, 200, GetColor(0, 0, 255), "countup:%d", countup);
 	switch (GameMain::NowStage)
 	{
@@ -223,43 +208,6 @@ void Enemy::Draw()
 			DrawCircle(s_enemy[i].enemyX + Stage3::Stage3X, s_enemy[i].enemyY, Range, GetColor(0, 255, 0), FALSE);
 
 		}
-
-		//if (s_enemy[0].Turnflg == 1)
-		//{
-		//	DrawTurnGraph(s_enemy[0].enemyX + Stage3::Stage3X - 30, s_enemy[0].enemyY - 30, E_Img, TRUE);
-		//}
-		//else if (s_enemy[0].Turnflg == 0)
-		//{
-		//	DrawGraph(s_enemy[0].enemyX + Stage3::Stage3X - 30, s_enemy[0].enemyY - 30, E_Img, TRUE);
-		//}
-
-		//DrawBox(ex + /*moveX +*/ Stage3::Stage3X, ey, ex2 + /*moveX +*/ Stage3::Stage3X, ey2, GetColor(0, 255, 0), FALSE);
-		//DrawCircle(s_enemy[0].enemyX + Stage3::Stage3X, s_enemy[0].enemyY, Range, GetColor(0, 255, 0), FALSE);
-
-		//if (s_enemy[1].Turnflg == 1)
-		//{
-		//	DrawTurnGraph(s_enemy[1].enemyX + Stage3::Stage3X - 30, s_enemy[1].enemyY - 30, E_Img, TRUE);
-		//}
-		//else if (s_enemy[1].Turnflg == 0)
-		//{
-		//	DrawGraph(s_enemy[1].enemyX + Stage3::Stage3X - 30, s_enemy[1].enemyY - 30, E_Img, TRUE);
-		//}
-
-		//DrawBox(ex + /*moveX +*/ Stage3::Stage3X, ey, ex2 + /*moveX +*/ Stage3::Stage3X, ey2, GetColor(0, 255, 0), FALSE);
-		//DrawCircle(s_enemy[1].enemyX + Stage3::Stage3X, s_enemy[1].enemyY, Range, GetColor(0, 255, 0), FALSE);
-
-		//if (s_enemy[2].Turnflg == 1)
-		//{
-		//	DrawTurnGraph(s_enemy[2].enemyX + Stage3::Stage3X - 30, s_enemy[2].enemyY - 30, E_Img, TRUE);
-		//}
-		//else if (s_enemy[2].Turnflg == 0)
-		//{
-		//	DrawGraph(s_enemy[2].enemyX + Stage3::Stage3X - 30, s_enemy[0].enemyY - 30, E_Img, TRUE);
-		//}
-
-		//DrawBox(ex + /*moveX +*/ Stage3::Stage3X, ey, ex2 + /*moveX +*/ Stage3::Stage3X, ey2, GetColor(0, 255, 0), FALSE);
-		//DrawCircle(s_enemy[2].enemyX + Stage3::Stage3X, s_enemy[2].enemyY, Range, GetColor(0, 255, 0), FALSE);
-
 		break;
 	}
 }
@@ -283,7 +231,6 @@ void Enemy::Enemyhit()
 			 if (c[0] <= Hitrange)
 			 {
 
-				 Hitflg = TRUE;
 				 Player::Life -= 1;
 				 Player::Death += 1;
 				 Player::playerX = 220;
@@ -292,11 +239,6 @@ void Enemy::Enemyhit()
 				 Stage2::Stage2X = 0;
 				 Stage3::Stage3X = 0;
 
-				 Hitflg = FALSE;
-			 }
-			 else
-			 {
-				 Hitflg = FALSE;
 			 }
 		 }
 		break;
@@ -317,7 +259,6 @@ void Enemy::Enemyhit()
 				if (c[i] <= Hitrange)
 				{
 
-					Hitflg = TRUE;
 					Player::Life -= 1;
 					Player::Death += 1;
 					Player::playerX = 220;
@@ -325,12 +266,7 @@ void Enemy::Enemyhit()
 					Stage1::Stage1X = 0;
 					Stage2::Stage2X = 0;
 					Stage3::Stage3X = 0;
-					Hitflg = FALSE;
 					break;
-				}
-				else
-				{
-					Hitflg = FALSE;
 				}
 			}
 		}
@@ -350,7 +286,6 @@ void Enemy::Enemyhit()
 			if (c[i] <= Hitrange)
 			{
 
-				Hitflg = TRUE;
 				Player::Life -= 1;
 				Player::Death += 1;
 				Player::playerX = 220;
@@ -358,12 +293,7 @@ void Enemy::Enemyhit()
 				Stage1::Stage1X = 0;
 				Stage2::Stage2X = 0;
 				Stage3::Stage3X = 0;
-				Hitflg = FALSE;
 				break;
-			}
-			else
-			{
-				Hitflg = FALSE;
 			}
 		}
 		 break;
